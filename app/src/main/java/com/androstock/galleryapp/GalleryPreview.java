88577d737a2e5566581e0cd8de01df5438a9eee2
Mahammad Thufail <eduguide0@gmail.com>
2019-10-12 15:50:45 +0530

    The Gallery App

---
+++ app/src/main/java/com/androstock/galleryapp/GalleryPreview.java
@@ -0,0 +1,31 @@
+package com.androstock.galleryapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import java.io.File;
+
+/**
+ * Created by SHAJIB on 25/12/2015.
+ */
+public class GalleryPreview extends AppCompatActivity {
+
+    ImageView GalleryPreviewImg;
+    String path;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.gallery_preview);
+        Intent intent = getIntent();
+        path = intent.getStringExtra("path");
+        GalleryPreviewImg = (ImageView) findViewById(R.id.GalleryPreviewImg);
+        Glide.with(GalleryPreview.this)
+                .load(new File(path)) // Uri of the picture
+                .into(GalleryPreviewImg);
+    }
+}
